ac719928611ae6cca79b2c0397c0236009c72ed9
Heidi Hlavinka <hchlav@gmail.com>
2018-03-04 23:08:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,12 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-
+def countdown_with_sleep(number)
+  number = 10
+    while number > 0
+      puts "#{number} SECOND(S)!"
+      sleep 1
+      number -= 1
+    end
+  "HAPPY NEW YEAR!"
 end
